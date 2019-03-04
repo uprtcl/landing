0fd3c49b6e86e8dd96c6a10db736ad0958e9442b
Pepo <pepo.ospina@gmail.com>
2019-03-04 18:37:38 +0100

    added content from C1

--- front/src/components/LandingPage.vue
+++ front/src/components/LandingPage.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="hello">
     <h1>_Protocol</h1>
+    <p>{{ topSentence }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'LandingPage',
+
   props: {
-    msg: String
+  },
+
+  data () {
+    return {
+      topSentence: ''
+    }
+  },
+
+  methods: {
+    updateContent () {
+      this.axios.get('http://www.collectiveone.org/1/model/section/ac109cd2-6939-1811-8169-399a03130000', {
+        params: {
+          levels: 2
+        }
+      }).then((response) => {
+        let subsections = response.data.data.subsectionsCommon
+        let topSection = subsections.find(s => s.title === 'Top Section')
+        let topCard = topSection.cardsWrappersCommon.find(c => c.card.title === 'Static sentence').card
+        this.topSentence = topCard.text
+      })
+    }
+  },
+
+  created () {
+    this.updateContent()
   }
+
 }
 </script>
 
