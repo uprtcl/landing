536e35de55d7cc2cf167e24b4177dd3cb01e944d
Pepo <pepo.ospina@gmail.com>
2019-03-04 10:45:00 +0100

    clean up and add vue

---
+++ front/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <app-landing-page></app-landing-page>
+  </div>
+</template>
+
+<script>
+import LandingPage from './components/LandingPage.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'app-landing-page': LandingPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
